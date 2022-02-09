88889eb8847f7740bc243f4985a9a2926ad26485
Bartek <kuzniewski.bar@gmail.com>
2022-02-09 16:03:11 +0100

    added variables

---
+++ main.js
@@ -0,0 +1,11 @@
+const username = document.querySelector('#username')
+const pass = document.querySelector('#password')
+const secondPass = document.querySelector('#secondPassword')
+const email = document.querySelector('#email')
+
+const clearBtn = document.querySelector('.clear')
+const sendBtn = document.querySelector('.send')
+const closeBtn = document.querySelector('.close')
+
+const errorMsg = document.querySelector('error-text')
+const popup = document.querySelector('.popup')
