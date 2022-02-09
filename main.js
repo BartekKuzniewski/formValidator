4bac27ad356006b971fb794ab61afb221b36ec40
Bartek <kuzniewski.bar@gmail.com>
2022-02-09 16:16:28 +0100

    addded function to clear all inputs

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const username = document.querySelector('#username')
-const pass = document.querySelector('#password')
-const secondPass = document.querySelector('#secondPassword')
-const email = document.querySelector('#email')
+const username = document.querySelector('#username');
+const pass = document.querySelector('#password');
+const secondPass = document.querySelector('#secondPassword');
+const email = document.querySelector('#email');
 
-const clearBtn = document.querySelector('.clear')
-const sendBtn = document.querySelector('.send')
-const closeBtn = document.querySelector('.close')
+const clearBtn = document.querySelector('.clear');
+const sendBtn = document.querySelector('.send');
+const closeBtn = document.querySelector('.close');
 
-const errorMsg = document.querySelector('error-text')
-const popup = document.querySelector('.popup')
+const errorMsg = document.querySelector('error-text');
+const popup = document.querySelector('.popup');
+
+clearBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+    [username, pass, secondPass, email].forEach( el => {
+		el.value = ''
+	});
+});
