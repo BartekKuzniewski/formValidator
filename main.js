4c70aef0407f9cc277322c894463966fc67d7889
Bartek <kuzniewski.bar@gmail.com>
2022-02-10 14:51:07 +0100

    added function thats checking length of username and password

--- main.js
+++ main.js
@@ -12,38 +12,50 @@ const popup = document.querySelector('.popup');
 
 const inputData = [username, pass, secondPass, email];
 
-
 const showError = (input, msg) => {
-    const formBox = input.parentElement;
-    const errorMsg = formBox.querySelector('.error-text')
+	const formBox = input.parentElement;
+	const errorMsg = formBox.querySelector('.error-text');
 
-    formBox.classList.add('error');
-    errorMsg.textContent = msg;
-}
+	formBox.classList.add('error');
+	errorMsg.textContent = msg;
+};
 
 const clearError = (input) => {
-    const formBox = input.parentElement;
-    formBox.classList.remove('error')
+	const formBox = input.parentElement;
+
+	formBox.classList.remove('error')
 }
 
 const checkForm = (input) => {
 	input.forEach((el) => {
 		if (el.value === '') {
-			showError(el, el.placeholder)
+			showError(el, el.placeholder);
+
 		} else {
 			clearError(el)
 		}
 	});
 };
 
+const checkLength = (input, minValue) => {
+		if(input.value.length < minValue){
+			showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} musi składać się z ${minValue} znaków`)
+		}
+}
+
+
+
 sendBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 
 	checkForm(inputData);
+	checkLength(username, 5)
+	checkLength(pass, 8)
 });
 
 clearBtn.addEventListener('click', (e) => {
 	e.preventDefault();
+
 	inputData.forEach((el) => {
 		el.value = '';
 	});
